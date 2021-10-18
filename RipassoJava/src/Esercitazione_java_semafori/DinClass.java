2cfcfd80e0aef368e338702ac6cca66b3c078c2e
Daniele002 <danielefassetta02@gmail.com>
2021-10-18 21:50:39 +0200

    Esercitazione_java_semafori

---
+++ RipassoJava/src/Esercitazione_java_semafori/DinClass.java
@@ -0,0 +1,11 @@
+package Esercitazione_java_semafori;
+
+public class DinClass implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RipassoJava/src/Esercitazione_java_semafori/MainClass.java
@@ -0,0 +1,10 @@
+package Esercitazione_java_semafori;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
